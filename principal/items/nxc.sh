859c3c2cc01e0c1a64587c154aeb0609159bcec0
S3Jon <sanchezeguiajon@gmail.com>
2026-04-28 14:14:18 +0200

    v1.3

--- principal/items/nxc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nxc ssh localhost:2222 -u users.txt -p 'D3pl0y_$$H_Now42!'
--- tools.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt install pipx git && pipx ensurepath && pipx install git+https://github.com/Pennyw0rth/NetExec && sudo apt install python3-argcomplete && register-python-argcomplete nxc >> ~/.bashrc
